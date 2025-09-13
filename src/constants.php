6f695dc53f2e03f6a037e5e66e1b0bc0958d828c
eremie523 <reremie523@gmail.com>
2025-09-13 23:15:42 +0100

    Testing out develop branch

--- src/constants.php
+++ src/constants.php
@@ -8,3 +8,4 @@ define('EMAIL_REGEX', "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/");
 define('PASSWORD_REGEX', "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};:\\'\"\\\\|,.<>\\/?]).{8,}$/");
 define('PERMITTED_LANGUAGES', ['en', 'es', 'fr', 'de', 'it', 'pt', 'ru', 'zh', 'ja']);
 define('PROFICIENCY_LEVELS', ['beginner', 'intermediate', 'advanced']);
+define('TESTING_BRANCH', "");
